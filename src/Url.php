a96067ba404de622fccb55fd969fc995b2de0b60
Dmitrii Ganin <d.ganin@kant.ru>
2021-12-07 07:50:54 +0400

    add method addPathItem

--- src/Url.php
+++ src/Url.php
@@ -3,12 +3,12 @@ namespace Askonadev;
 
 class Url
 {
-    private string $sPath;
-    private array  $arParams = [];
+    private array $arParams = [];
+    private array $arPathItems = [];
     
     public function __construct(string $sUrl)
     {
-        $this->sPath = $sUrl;
+        $this->arPathItems = array_filter(explode("/", $sUrl));
     }
     
     public function addParam(string $sKey, $value): void
@@ -23,6 +23,11 @@ class Url
         }
     }
     
+    public function addPathItem(string $sPathItem): void
+    {
+        $this->arPathItems = array_merge($this->arPathItems, array_filter(explode("/", $sPathItem)));
+    }
+    
     public function removeParam(string $sKey): void
     {
         unset($this->arParams[$sKey]);
@@ -30,7 +35,8 @@ class Url
     
     public function toString(): string
     {
-        $sReturnUrl = $this->sPath;
+        $sReturnUrl = $this->getPath();
+        
         if (!empty($this->arParams)) {
             $sReturnUrl .= "?".str_replace(["%5B", "%5D"], ["[", "]"], http_build_query($this->arParams));
         }
@@ -38,4 +44,8 @@ class Url
         return $sReturnUrl;
     }
     
+    private function getPath(): string
+    {
+        return "/".implode("/", $this->arPathItems)."/";
+    }
 }
